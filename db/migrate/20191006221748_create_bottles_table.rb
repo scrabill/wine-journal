870c2eba4103398a9f8df14c75ab4643b558b945
scrabill <shannon.crabill@gmail.com>
2019-10-06 18:21:43 -0400

    Creates bottle table with name, brand, type, description and note columns

--- db/migrate/20191006221748_create_bottles_table.rb
+++ db/migrate/20191006221748_create_bottles_table.rb
@@ -1,4 +1,11 @@
 class CreateBottlesTable < ActiveRecord::Migration
   def change
+    create_table :bottles do |t|
+      t.string :name
+      t.string :brand
+      t.string :type
+      t.text :description
+      t.text :note
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191006221014) do
+ActiveRecord::Schema.define(version: 20191006221748) do
+
+  create_table "bottles", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.string "type"
+    t.text   "description"
+    t.text   "note"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
