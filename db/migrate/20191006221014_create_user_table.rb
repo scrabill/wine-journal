05cea02d47f5bc9c0ebe405f6175006e28ff0304
scrabill <shannon.crabill@gmail.com>
2019-10-06 18:12:51 -0400

    Creates user table with name, email and password columns

--- db/migrate/20191006221014_create_user_table.rb
+++ db/migrate/20191006221014_create_user_table.rb
@@ -1,4 +1,9 @@
 class CreateUserTable < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191006221014) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
 
 end
