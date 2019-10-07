50b71223f6f08d9d12882c227b76934dbabe37b2
scrabill <shannon.crabill@gmail.com>
2019-10-07 19:47:16 -0400

    Seeds database with wine bottles for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Bottle.create(:name => "19 Crimes Shiraz", :type => "Red", :brand => "19 Crimes", :description => "Affordable! Great for parties."  )
-Bottle.create(:name => "Sangria", :type => "Red", :brand => "Carlo Rossi", :description => "So good! Good at room temp and chilled."  )
-Bottle.create(:name => "Pfalz Riesling", :type => "White", :brand => "Cupcake Vineyards", :description => "Not my favorite, but worth trying again. It's a good one for summer and pairs well with cheese.")
+Bottle.create(:name => "19 Crimes Shiraz", :variety => "Red", :brand => "19 Crimes", :description => "Affordable! Great for parties."  )
+Bottle.create(:name => "Sangria", :variety => "Red", :brand => "Carlo Rossi", :description => "So good! Good at room temp and chilled."  )
+Bottle.create(:name => "Pfalz Riesling", :variety => "White", :brand => "Cupcake Vineyards", :description => "Not my favorite, but worth trying again. It's a good one for summer and pairs well with cheese.")
