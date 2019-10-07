ac800b705962ab4c2e4d96d42c47cdea9565863a
scrabill <shannon.crabill@gmail.com>
2019-10-06 22:15:53 -0400

    Adds three wine bottles objects and saves them to the database

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Bottle.create(:name => "19 Crimes Shiraz", :type => "Red", :brand => "19 Crimes", :description => "Affordable! Great for parties."  )
+Bottle.create(:name => "Sangria", :type => "Red", :brand => "Carlo Rossi", :description => "So good! Good at room temp and chilled."  )
+Bottle.create(:name => "Pfalz Riesling", :type => "White", :brand => "Cupcake Vineyards", :description => "Not my favorite, but worth trying again. It's a good one for summer and pairs well with cheese.")
