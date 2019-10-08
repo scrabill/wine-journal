2e8b51a1fcc758f89828075c6a58ab03f19f6da2
scrabill <shannon.crabill@gmail.com>
2019-10-07 20:37:26 -0400

    Reverts sessions commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "wine-journal"
   end
 
   get "/" do
