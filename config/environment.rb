578c3543b2cedf74ee589b1184089bccdcf68e0c
scrabill <shannon.crabill@gmail.com>
2019-10-09 16:48:44 -0400

    Adds dotenv gem for managing environmental variables

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 ENV['SINATRA_ENV'] ||= "development"
+# ENV['RACK_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
@@ -7,6 +8,9 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+require 'dotenv'
+Dotenv.load
 
 require './app/controllers/application_controller'
 require_all 'app'
