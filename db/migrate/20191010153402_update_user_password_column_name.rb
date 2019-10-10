1343a6b5a6b978637f9aa17a25d88d8e76c84fac
scrabill <shannon.crabill@gmail.com>
2019-10-10 11:37:39 -0400

    Updates user password column name to password_digest

---
+++ db/migrate/20191010153402_update_user_password_column_name.rb
@@ -0,0 +1,5 @@
+class UpdateUserPasswordColumnName < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191009220234) do
+ActiveRecord::Schema.define(version: 20191010153402) do
 
   create_table "bottles", force: :cascade do |t|
     t.string  "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20191009220234) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
